b385e1e89dddcb7223605e09e64ee3de517a579e
mondedelotus <mondedelotus@gmail.com>
2023-05-22 05:31:50 +0300

    changed header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="header__wrapper">
             <h1 className="header__title">
                 <strong>Hi, my name is <em>Serhii</em></strong><br />
-                a frontend developer
+                a web developer
             </h1>
             <div className="header__text">
                 <p>with passion for learning and creating.</p>
