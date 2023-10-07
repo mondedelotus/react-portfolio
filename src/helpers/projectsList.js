7d4c4a006c6f702a022ed9cff12c840dd2e578ed
mondedelotus <mondedelotus@gmail.com>
2023-10-07 22:28:21 +0300

    added task manager project

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -14,7 +14,6 @@ import selfieboothBig from "./../img/projects/real/selfieboothco-big.webp";
 import nona from "./../img/projects/real/nonaphotographyca.webp";
 import nonaBig from "./../img/projects/real/nonaphotographyca-big.webp";
 
-
 // TEST
 import funiro from "./../img/projects/test/funiro.webp";
 import funiroBig from "./../img/projects/test/funiro-big.webp";
@@ -25,77 +24,86 @@ import portenBig from "./../img/projects/test/porten-big.webp";
 // import myTeam from "./../img/projects/test/my-team.webp";
 // import myTeamBig from "./../img/projects/test/my-team-big.webp";
 
-import interior from "./../img/projects/test/interior-cons-2.webp";
-import interiorBig from "./../img/projects/test/interior-cons-big.webp";
+// import interior from "./../img/projects/test/interior-cons-2.webp";
+// import interiorBig from "./../img/projects/test/interior-cons-big.webp";
+
+import taskManager from "./../img/projects/test/task-manager.webp";
+import taskManagerBig from "./../img/projects/test/task-manager-big.webp";
 
 const projects = [
-	{
-		title: 'ContentAdore',
-		skills: 'WordPress, PHP, VueJS (+ Vuetify), HTML, CSS (+ Bootstrap 4), JS, Swiper',
-		img: ca,
-		imgBig: caBig,
-		projectLink: 'https://contentadore.com/',
-	},
-	{
-		title: 'HeatSign',
-		img: heatsign,
-		imgBig: heatsignBig,
-		skills: 'WordPress, PHP, Woocommerce, HTML, CSS',
-		projectLink: 'https://www.heatsign.com/',
-	},
-	{
-		title: 'BuyselfieBooth',
-		img: buyselfie,
-		imgBig: buyselfieBig,
-		skills: 'WordPress, PHP, Woocommerce, HTML, CSS, JS',
-		projectLink: 'https://buyselfiebooth.com/',
-	},
-	{
-		title: 'SelfieBoothCo',
-		img: selfiebooth,
-		imgBig: selfieboothBig,
-		skills: 'WordPress, PHP, HTML, CSS, JS',
-		projectLink: 'https://selfieboothco.com/',
-	},
-	{
-		title: 'Nona Photography',
-		img: nona,
-		imgBig: nonaBig,
-		skills: 'WordPress, PHP, HTML, CSS, JS',
-		projectLink: 'https://nonaphotographyca.com/',
-	},
-
+    {
+        title: "ContentAdore",
+        skills: "WordPress, PHP, VueJS (+ Vuetify), HTML, CSS (+ Bootstrap 4), JS, Swiper",
+        img: ca,
+        imgBig: caBig,
+        projectLink: "https://contentadore.com/",
+    },
+    {
+        title: "HeatSign",
+        img: heatsign,
+        imgBig: heatsignBig,
+        skills: "WordPress, PHP, Woocommerce, HTML, CSS",
+        projectLink: "https://www.heatsign.com/",
+    },
+    {
+        title: "BuyselfieBooth",
+        img: buyselfie,
+        imgBig: buyselfieBig,
+        skills: "WordPress, PHP, Woocommerce, HTML, CSS, JS",
+        projectLink: "https://buyselfiebooth.com/",
+    },
+    {
+        title: "SelfieBoothCo",
+        img: selfiebooth,
+        imgBig: selfieboothBig,
+        skills: "WordPress, PHP, HTML, CSS, JS",
+        projectLink: "https://selfieboothco.com/",
+    },
+    {
+        title: "Nona Photography",
+        img: nona,
+        imgBig: nonaBig,
+        skills: "WordPress, PHP, HTML, CSS, JS",
+        projectLink: "https://nonaphotographyca.com/",
+    },
 ];
 
 const testProjects = [
-	{
-		title: 'Funiro landing',
-		img: funiro,
-		imgBig: funiroBig,
-		skills: 'HTML, SCSS (with BEM), JS, Swiper, Lightgallery, Gulp',
-		projectLink: 'https://funiro-two.vercel.app/',
-	},
-	{
-		title: 'Porten landing',
-		img: porten,
-		imgBig: portenBig,
-		skills: 'HTML, SCSS (with BEM), JS, Swiper',
-		projectLink: 'https://mondedelotus.github.io/spacelab-1-landing/',
-	},
-	// {
-	// 	title: 'My team sample',
-	// 	img: myTeam,
-	// 	imgBig: myTeamBig,
-	// 	skills: 'HTML, SCSS (with BEM)',
-	// 	projectLink: 'https://mondedelotus.github.io/my-team-page/',
-	// },
-	{
-		title: 'Interior consultant sample',
-		img: interior,
-		imgBig: interiorBig,
-		skills: 'HTML, SCSS (with BEM), JS',
-		projectLink: 'https://mondedelotus.github.io/interior-consultant/',
-	},
-] 
+    {
+        title: "Funiro landing",
+        img: funiro,
+        imgBig: funiroBig,
+        skills: "HTML, SCSS (with BEM), JS, Swiper, Lightgallery, Gulp",
+        projectLink: "https://funiro-two.vercel.app/",
+    },
+    {
+        title: "Porten landing",
+        img: porten,
+        imgBig: portenBig,
+        skills: "HTML, SCSS (with BEM), JS, Swiper",
+        projectLink: "https://mondedelotus.github.io/spacelab-1-landing/",
+    },
+    // {
+    // 	title: 'My team sample',
+    // 	img: myTeam,
+    // 	imgBig: myTeamBig,
+    // 	skills: 'HTML, SCSS (with BEM)',
+    // 	projectLink: 'https://mondedelotus.github.io/my-team-page/',
+    // },
+    {
+        title: "Task Manager App",
+        img: taskManager,
+        imgBig: taskManagerBig,
+        skills: "React, Redux, CSS Modules, SCSS, Framer Motion, Vite, Vercel",
+        projectLink: "https://task-manager-lac-three.vercel.app/",
+    },
+    // {
+    //     title: "Interior consultant sample",
+    //     img: interior,
+    //     imgBig: interiorBig,
+    //     skills: "HTML, SCSS (with BEM), JS",
+    //     projectLink: "https://mondedelotus.github.io/interior-consultant/",
+    // },
+];
 
-export {projects, testProjects}
+export { projects, testProjects };
