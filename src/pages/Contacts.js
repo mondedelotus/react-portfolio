34204fa2bfdbb2b284d80e29d94c723835bccb51
mondedelotus <mondedelotus@gmail.com>
2023-10-09 01:28:15 +0300

    updated location in contacts info

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,27 +1,30 @@
 const Contacts = () => {
-    return ( 
+    return (
         <main className="section">
             <div className="container">
-                    <h1 className="title-1">Contacts</h1>
+                <h1 className="title-1">Contacts</h1>
 
-                    <ul className="content-list">
-                        <li className="content-list__item">
-                            <h2 className="title-2">Location</h2>
-                            <p>Odesa, Ukraine</p>
-                        </li>
-                        <li className="content-list__item">
-                            <h2 className="title-2">Telegram</h2>
-                            <p>@serhiogoa</p>
-                        </li>
-                        <li className="content-list__item">
-                            <h2 className="title-2">Email</h2>
-                            <p><a href="mailto:sergio.argos@proton.me">sergio.argos@proton.me</a></p>
-                        </li>
-                    </ul>
-
+                <ul className="content-list">
+                    <li className="content-list__item">
+                        <h2 className="title-2">Location</h2>
+                        <p>Warsaw soon</p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Telegram</h2>
+                        <p>@serhiogoa</p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Email</h2>
+                        <p>
+                            <a href="mailto:sergio.argos@proton.me">
+                                sergio.argos@proton.me
+                            </a>
+                        </p>
+                    </li>
+                </ul>
             </div>
         </main>
-     );
-}
- 
+    );
+};
+
 export default Contacts;
