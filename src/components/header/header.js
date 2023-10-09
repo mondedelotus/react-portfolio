bf0be209d46cc5cdd8f61857cdf19a71b1b84e20
mondedelotus <mondedelotus@gmail.com>
2023-10-10 01:27:06 +0300

    added my picture to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,20 +1,36 @@
 import "./styles.css";
 
+import me from "./../../img/me.webp";
+
 const Header = () => {
-  return ( 
-    <header className="header">
-        <div className="header__wrapper">
-            <h1 className="header__title">
-                <strong>Hi, my name is <em>Serhii</em></strong><br />
-                a web developer
-            </h1>
-            <div className="header__text">
-                <p>with passion for learning and creating.</p>
+    return (
+        <header className="header">
+            <div className="header__wrapper">
+                <div className="content-part">
+                    <h1 className="header__title">
+                        <strong>
+                            Hi, my name is <em>Serhii</em>
+                        </strong>
+                        <br />a web developer
+                    </h1>
+                    <div className="header__text">
+                        <p>with passion for learning and creating.</p>
+                    </div>
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://docs.google.com/document/d/1_ikJCpuJd_Of9Bn8HAg_Y7IpU4P02wtBozkuMtce-lw/edit?usp=sharing"
+                        className="btn"
+                    >
+                        Download CV
+                    </a>
+                </div>
+                <div className="picture-part">
+                    <img src={me} alt="me" width="468" height="533" />
+                </div>
             </div>
-            <a target="_blank" href="https://docs.google.com/document/d/1_ikJCpuJd_Of9Bn8HAg_Y7IpU4P02wtBozkuMtce-lw/edit?usp=sharing" className="btn">Download CV</a>
-        </div>
-    </header>
-   );
-}
- 
+        </header>
+    );
+};
+
 export default Header;
